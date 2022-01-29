0dbdabac4066f463abf605994292403a78de3628
tdhooghe <thomas.dhooghe95@gmail.com>
2022-01-29 13:35:42 +0100

    updated results

--- results/results.py
+++ results/results.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-#%%
+# %%
 cwd = os.getcwd()
 print(cwd)
 
@@ -10,15 +10,14 @@ print(cwd)
 columns = ['task', 'framework', 'constraint', 'fold', 'type', 'result', 'metric', 'duration', 'training_duration',
            'models_count']
 results = pd.read_csv("./results/results.csv", usecols=columns)
-results = results[results['constraint'].str.contains("1h12c") == False]
+results = results[~results.constraint.str.contains("1h12c")]
 results = results[results.constraint != 'test']
-results = results.groupby(['framework', 'task']).agg({'result': [np.mean, np.std], })
+results['task'] = results['task'].str.lower()
+agg_model_counts = results.groupby(['framework', 'type'], observed=True).agg({'models_count': [np.mean]})
+results = results.groupby(['framework', 'type', 'task'], observed=True).agg({'result': [np.mean, np.std]})
 
+results.columns = ['mean', 'std']
+results = results.reset_index()
+results['mean'] = np.round(results['mean'], decimals=3)
+results['std'] = np.round(results['std'], decimals=4)
 print(results)
-#results = results.groupby('task')['result'].agg([np.mean, np.std])
-
-#%%
-#results_hyperboost = results_hyperboost.groupby('task').agg(lambda x: list(x))
-results_hyperboost = results_hyperboost.groupby(['framework', 'task']).agg({'result': [np.mean, np.std], })
-
-print(results_hyperboost)
